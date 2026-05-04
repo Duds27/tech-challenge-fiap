dee4da58d2fc2e8b580a7b82e4fce0d2dc8756fc
Eduardo Encarnacao <eduardo@mail.com>
2026-05-04 20:16:02 -0300

    First version running

--- OficinaMecanicaBackend/src/OficinaMecanicaBackend/Program.cs
+++ OficinaMecanicaBackend/src/OficinaMecanicaBackend/Program.cs
@@ -1,25 +1,80 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+using Microsoft.EntityFrameworkCore;
+using OficinaMecanicaBackend.Data;
+using Serilog;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/app-.log", rollingInterval: RollingInterval.Day)
+    .Enrich.FromLogContext()
+    .CreateBootstrapLogger();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Use Serilog for logging
+    builder.Host.UseSerilog((ctx, services, cfg) => cfg
+        .ReadFrom.Configuration(ctx.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.File("logs/app-.log", rollingInterval: RollingInterval.Day));
+
+    // EF Core + MySQL
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+    // Add services to the container.
+
+    // Controllers
+    builder.Services.AddControllers();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "OficinaMecanicaBackend API", Version = "v1" });
+    });
+
+    var app = builder.Build();
+
+    // Apply pending EF Core migrations automatically on startup
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        db.Database.Migrate();
+    }
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseSerilogRequestLogging();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
+}
+catch (HostAbortedException ex)
+{
+    throw;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+// Expose for WebApplicationFactory in tests
+public partial class Program { }
--- OficinaMecanicaBackend/src/OficinaMecanicaBackend/WeatherForecast.cs
+++ OficinaMecanicaBackend/src/OficinaMecanicaBackend/WeatherForecast.cs
@@ -1,7 +1,9 @@
-namespace OficinaMecanicaBackend;
+namespace OficinaMecanicaBackend.Models;
 
 public class WeatherForecast
 {
+    public int Id { get; set; }
+
     public DateOnly Date { get; set; }
 
     public int TemperatureC { get; set; }
